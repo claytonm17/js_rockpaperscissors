cef585c033b057f94c7b115f1f1146a87cd1e18e
claytonm17 <133607487+claytonm17@users.noreply.github.com>
2023-10-26 10:15:12 -0400

    Create logic for first to 5 wins
    Add scoreboard to keep track of score

--- rps.js
+++ rps.js
@@ -5,7 +5,7 @@ const rock = document.querySelector('#rock');
 rock.addEventListener('click', () => {
 const user = 'rock';
 // Run the game
-game(user);
+playRound(user);
 });
 
 // User chooses paper
@@ -13,7 +13,7 @@ const paper = document.querySelector('#paper');
 paper.addEventListener('click', () => {
 const user = 'paper';
 // Run the game
-game(user);
+playRound(user);
 });
 
 // User chooses scissors
@@ -21,7 +21,7 @@ const scissors = document.querySelector('#scissors');
 scissors.addEventListener('click', () => {
 const user = 'scissors';
 // Run the game
-game(user);
+playRound(user);
 });
 
 // Computer choice function using random number generator
@@ -38,20 +38,40 @@ function getComputerChoice() {
     }
 
 // Function to update the result message
+const results = document.querySelector('#results');
 function updateResults(message) {
-    const results = document.querySelector('#results');
+    // Return new
+    results.innerHTML = '';
+
+    // Create and append new message
     const content = document.createElement('div');
     content.classList.add('content');
     content.textContent = message;
     results.appendChild(content);
 }
 
-// Running the match
-function game(userChoice) {
+// Function for the scoreboard to be updated
+function updateScoreboard(score) {
+    const content = document.createElement('div');
+    content.classList.add('score');
+    content.textContent = score;
+    results.appendChild(content);
+}
 
-    computerChoice = getComputerChoice();
+function continueGame() {
+    return userScore < 5 && computerScore < 5;
+}
 
-    console.log(userChoice, computerChoice)
+// Initialize empty scores
+let userScore = 0;
+let computerScore = 0;
+
+// Run the round
+function playRound(userChoice) {
+
+    const computerChoice = getComputerChoice();
+
+    //console.log(userChoice, computerChoice)
 
     // Condition if game is a tie
     if (userChoice == computerChoice) {
@@ -64,9 +84,11 @@ function game(userChoice) {
         // DOM Manipulation to create div inside results div
         if (computerChoice === "paper") {
             updateResults('Computer chose paper. You lose!');
+            computerScore++;
 
         } else if (computerChoice === "scissors") {
             updateResults('Computer chose scissors. You win!');
+            userScore++;
         }
         
     // Conditions if user chooses paper
@@ -74,9 +96,11 @@ function game(userChoice) {
 
         if (computerChoice === "scissors") {
             updateResults("Computer chose scissors. You lose!");
+            computerScore++;
 
         } else if (computerChoice === "rock") {
             updateResults("Computer chose rock. You win!");
+            userScore++;
         }
 
     // Conditions if user chooses scissors
@@ -84,9 +108,30 @@ function game(userChoice) {
 
         if (computerChoice === "rock") {
             updateResults("Computer chose rock. You lose!");
+            computerScore++;
 
         } else if (computerChoice === "paper") {
             updateResults("Computer chose paper. You win!");
+            userScore++;
         }
         }
+    
+    // Scorecard
+    score = `User: ${userScore} - Computer: ${computerScore}`;
+    updateScoreboard(score);
+
+    if (userScore === 5 || computerScore === 5) {
+        endGame();
     }
+}
+    
+function endGame() {
+    if (userScore === 5) {
+        updateResults('You win the game!');
+        updateScoreboard(score);
+
+    } else if (computerScore === 5) {
+        updateResults('The computer won the game!');
+        updateScoreboard(score);
+    }
+}
